b8ca70e5128284649d00137c86399b3676df1f64
yc_machine <kebai0624@gmail.com>
2017-11-30 03:17:21 -0500

    exp4.1

--- omie_gan/gan.py
+++ omie_gan/gan.py
@@ -1,2 +1,26 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets.samples_generator import make_swiss_roll
+from sklearn.cluster import AgglomerativeClustering
+import mpl_toolkits.mplot3d.axes3d as p3
+import time as time
+
+def data_generator():
+	n_samples = 1500
+	noise = 0.1
+	X, _ = make_swiss_roll(n_samples, noise,random_state=4)
+	# Make it thinner
+	X[:, 0] /= 10
+	X[:, 2] /= 10
+	x1 = np.array([X[:,0],X[:,2]])
+	x2 = np.array([-X[:,0],-X[:,2]])
+	return np.concatenate([x1,x2],1)
+	
+
+
+
+# data = data_generator()
+# a = 1
+# plt.scatter(data[0,:],data[1,:])
--- omie_gan/mutual_info.py
+++ omie_gan/mutual_info.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 
 def data_generator():
 	mean = [0,0]
-	rou = 0.5
+
 	std1 = 1
 	std2 = 1
 	sample_points = mb_size
@@ -22,14 +22,15 @@ mb_size = 1000
 Z_dim = 2
 X_dim = 1 # mutual information
 # y_dim = 1
-h_dim = 10
+h_dim = 20
 c = 0
-lr = 1e-5
+rou = 0.2
+lr = 1e-3
 
 
 def xavier_init(size):
 	in_dim = size[0]
-	xavier_stddev = 1. / np.sqrt(in_dim / 2.)
+	xavier_stddev = 1. / np.sqrt((in_dim+h_dim) / 2.)
 	return Variable(torch.randn(*size) * xavier_stddev, requires_grad=True)
 
 
@@ -55,29 +56,51 @@ def reset_grad():
 			data = p.grad.data
 			p.grad = Variable(data.new().resize_as_(data).zero_())
 
-
+# T_solver =
 T_solver = optim.Adam(T_params, lr)
 
-iteration = 100000
-v_history = []
 
+# np.arange()
+# rou_list = np.array(range(10))/10
+mi_list = []
 
-for it in range(iteration):
-	x,y,y_hat = data_generator()
-	xy = Variable(torch.from_numpy(np.array([x.astype('float32'),y.astype('float32')]).T))
-	xy_hat = Variable(torch.from_numpy(np.array([x.astype('float32'),y_hat.astype('float32')]).T))
-
-	t1 = T(xy)
-	t2 = T(xy_hat)
-	v = torch.mean(t1) - torch.log(torch.mean(torch.exp(t2)))
-	v = -v
-
-	v_history.append(-v)
-	v.backward()
-	T_solver.step()
-
-	if it % 1000 == 0:
-		print('Iter: {}: mutual_information: {}, '.format(it, v))
-
-
-
+iteration = 30000
+for rou in np.arange(0,1,0.1):
+	it = 0
+	v_history = []
+	mean_v = 0
+	for it in range(iteration):
+		x,y,y_hat = data_generator()
+		xy = Variable(torch.from_numpy(np.array([x.astype('float32'),y.astype('float32')]).T))
+		xy_hat = Variable(torch.from_numpy(np.array([x.astype('float32'),y_hat.astype('float32')]).T))
+	
+		t1 = T(xy)
+		t2 = T(xy_hat)
+		v = torch.mean(t1) - torch.log(torch.mean(torch.exp(t2)))
+		v = -v
+		mean_v += v.data.tolist()[0]
+	
+		v.backward()
+		T_solver.step()
+		reset_grad()
+	
+		if it % 1000 == 0:
+			v_history.append(-mean_v/1000)
+			mean_v=0
+			print('Iter: {}: mutual_information: {}, '.format(it, v.data.tolist()[0]))
+	
+	mi_list.append(v_history[-1])
+	
+	x = range(len(v_history))
+	
+	fig1 = plt.plot(x,v_history)
+	plt.xlabel('iteration(*1000)')
+	plt.ylabel('mutual information')
+	plt.title("Mutual Information")
+	# plt.show()
+	# plt.legend('{}')
+	plt.savefig('{}_mutual_information.png'.format(str(rou)))
+	np.save('{}_mutual_information'.format(str(rou)),v_history[-1])
+	
+	
+	a = 1
